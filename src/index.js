5ef358b8f64d67e2b99d0a848ed0e4036f2a2915
Talenta Nkhatu <talentankhatu23@gmail.com>
2025-05-02 09:18:43 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-function handleSearchForm(event) {
-  event.preventDefault;
-  let searchInput = document.querySelector("#search-form-input");
+function displayTemperature(response) {
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "8944f371f4bfa78290t3baa86a330do0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}}&unit=metrics`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSearchForm(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchForm);
+
+searchCity("Paris");
