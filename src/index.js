01e2a82685e895752e271424ad404b054ba1617e
Talenta Nkhatu <talentankhatu23@gmail.com>
2025-05-03 18:37:46 +0200

    Inserted more weather data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 function showTemperature(response) {
   let tempElement = document.querySelector("#temp");
-
   let temperature = response.data.temperature.current;
   tempElement.innerHTML = Math.round(temperature);
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = `, ${response.data.condition.description}`;
+  console.log(response);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%,`;
+
+  let windspeedElement = document.querySelector("#wind-speed");
+  windspeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formattedDay(date);
+}
+
+function formattedDay(date) {
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  let day = days[date.getDay()];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${month} ${date.getDate()} ${date.getFullYear()} ${hours}:${minutes}`;
 }
 
 function handleSearchForm(event) {
